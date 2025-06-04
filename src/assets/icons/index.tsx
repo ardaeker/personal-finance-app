7ae76a3f602b6f03fa6a4444c25900b5185e4533
Arda Eker <96771398+ardaeker@users.noreply.github.com>
2025-06-04 04:00:59 +0300

    feat: add SVG icons for caret, dots, list, jar, magnifying glass, house, arrows, chart, potted plant, receipt, arrow, check, warning, wrench, network, video, barbell, music note, book, shield, warehouse, filter, sort, eye, and eye slash

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,236 @@
+export const CaretLeft = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="m10.147 3.146-5 5a.5.5 0 0 0 0 .708l5 5A.5.5 0 0 0 11 13.5v-10a.5.5 0 0 0-.853-.354Z" />
+    </svg>
+  );
+};
+
+export const CaretRight = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="m5.853 3.146 5 5a.5.5 0 0 1 0 .708l-5 5A.5.5 0 0 1 5 13.5v-10a.5.5 0 0 1 .853-.354Z" />
+    </svg>
+  );
+};
+
+export const CaretDown = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="m13.354 6.354-5 5a.502.502 0 0 1-.708 0l-5-5A.5.5 0 0 1 3 5.5h10a.5.5 0 0 1 .354.854Z" />
+    </svg>
+  );
+};
+
+export const CaretUp = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="m2.646 10.647 5-5a.5.5 0 0 1 .708 0l5 5A.5.5 0 0 1 13 11.5H3a.5.5 0 0 1-.354-.853Z" />
+    </svg>
+  );
+};
+
+export const DotsThree = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M9.75 8a1.75 1.75 0 1 1-3.5 0 1.75 1.75 0 0 1 3.5 0ZM3 6.25a1.75 1.75 0 1 0 0 3.5 1.75 1.75 0 0 0 0-3.5Zm10 0a1.75 1.75 0 1 0 0 3.5 1.75 1.75 0 0 0 0-3.5Z" />
+    </svg>
+  );
+};
+
+export const ListBullets = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M3.5 8a1 1 0 1 1-2 0 1 1 0 0 1 2 0Zm-1-5a1 1 0 1 0 0 2 1 1 0 0 0 0-2Zm0 8a1 1 0 1 0 0 2 1 1 0 0 0 0-2Zm11-4h-8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5Zm0-4h-8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5Zm0 8h-8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5Z" />
+    </svg>
+  );
+};
+
+export const JarFill = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M11.5 3.05V2a1 1 0 0 0-1-1h-5a1 1 0 0 0-1 1v1.05a2.503 2.503 0 0 0-2 2.45v7A2.5 2.5 0 0 0 5 15h6a2.5 2.5 0 0 0 2.5-2.5v-7a2.503 2.503 0 0 0-2-2.45ZM7.5 2h1v1h-1V2Zm-2 0h1v1h-1V2Zm3 9.5v.5a.5.5 0 0 1-1 0v-.5H7a.5.5 0 0 1 0-1h1.5a.5.5 0 0 0 0-1h-1a1.5 1.5 0 0 1 0-3V6a.5.5 0 1 1 1 0v.5H9a.5.5 0 1 1 0 1H7.5a.5.5 0 1 0 0 1h1a1.5 1.5 0 0 1 0 3Zm2-8.5h-1V2h1v1Z" />
+    </svg>
+  );
+};
+
+export const MagnifyingGlass = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="m14.354 13.646-3.13-3.129a5.507 5.507 0 1 0-.707.707l3.13 3.13a.5.5 0 0 0 .707-.708ZM2.5 7a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0Z" />
+    </svg>
+  );
+};
+
+export const House = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path d="M14 7.222V13a1 1 0 0 1-1 1h-2.5a1 1 0 0 1-1-1v-2.5A.5.5 0 0 0 9 10H7a.5.5 0 0 0-.5.5V13a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V7.222a1 1 0 0 1 .323-.736l5-4.718.007-.007a1 1 0 0 1 1.352.007l5 4.718a1 1 0 0 1 .318.736Z" />
+    </svg>
+  );
+};
+
+export const ArrowsDownUp = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path
+        fill="#201F24"
+        d="M7.462 10.809a.5.5 0 0 1-.108.545l-2 2a.5.5 0 0 1-.708 0l-2-2A.5.5 0 0 1 3 10.5h1.5V3a.5.5 0 0 1 1 0v7.5H7a.5.5 0 0 1 .462.309Zm5.892-6.163-2-2a.5.5 0 0 0-.708 0l-2 2A.5.5 0 0 0 9 5.5h1.5V13a.5.5 0 0 0 1 0V5.5H13a.5.5 0 0 0 .354-.854Z"
+      />
+    </svg>
+  );
+};
+
+export const ChartDonut = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M1.547 7.221a6.394 6.394 0 0 1 1.457-3.36 1 1 0 0 1 1.477-.069L6.21 5.56a.992.992 0 0 1 .106 1.28 1.6 1.6 0 0 0-.232.484.25.25 0 0 1-.238.177h-4.05a.25.25 0 0 1-.25-.279Zm7.04-5.717A1 1 0 0 0 7.5 2.5v2.542a.991.991 0 0 0 .828.985 2 2 0 0 1 .338 3.86.254.254 0 0 0-.166.238v4.077a.25.25 0 0 0 .279.25 6.52 6.52 0 0 0 5.721-6.36c.047-3.393-2.55-6.287-5.913-6.588ZM7.328 9.885a2 2 0 0 1-1.212-1.214.254.254 0 0 0-.238-.171H1.795a.25.25 0 0 0-.25.278 6.507 6.507 0 0 0 5.676 5.676.25.25 0 0 0 .278-.25v-4.079a.253.253 0 0 0-.17-.24Z" />
+    </svg>
+  );
+};
+
+export const PottedPlant = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M12.5 9H7.706l1.4-1.4a3.722 3.722 0 0 0 1.632.397c.57.001 1.13-.155 1.618-.451 1.483-.898 2.277-2.975 2.125-5.558a.5.5 0 0 0-.47-.47c-2.582-.152-4.66.643-5.558 2.125-.584.966-.6 2.132-.054 3.25l-.899.9-.763-.764c.375-.828.348-1.687-.087-2.405-.68-1.124-2.237-1.723-4.167-1.61a.5.5 0 0 0-.47.47C1.9 5.411 2.5 6.97 3.626 7.65c.376.23.809.35 1.25.35.4-.004.795-.093 1.157-.262l.761.762-.5.5H3.5a.5.5 0 0 0 0 1h.6l.825 3.717a.993.993 0 0 0 .976.783H10.1a.995.995 0 0 0 .977-.783L11.9 10h.599a.5.5 0 0 0 0-1Z" />
+    </svg>
+  );
+};
+
+export const Receipt = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M13.5 2.5h-11a1 1 0 0 0-1 1V13a.5.5 0 0 0 .724.447L4 12.559l1.776.888a.5.5 0 0 0 .448 0L8 12.559l1.776.888a.5.5 0 0 0 .448 0L12 12.559l1.776.888A.5.5 0 0 0 14.5 13V3.5a1 1 0 0 0-1-1ZM11 9H5a.5.5 0 1 1 0-1h6a.5.5 0 0 1 0 1Zm0-2H5a.5.5 0 1 1 0-1h6a.5.5 0 0 1 0 1Z" />
+    </svg>
+  );
+};
+
+export const ArrowFatLinesLeft = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M10 5v6a.5.5 0 0 1-.5.5H8V14a.5.5 0 0 1-.854.354l-6-6a.5.5 0 0 1 0-.708l6-6A.5.5 0 0 1 8 2v2.5h1.5a.5.5 0 0 1 .5.5Zm1.5-.5a.5.5 0 0 0-.5.5v6a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Zm2 0a.5.5 0 0 0-.5.5v6a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
+    </svg>
+  );
+};
+
+export const CheckCircle = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M8 1.5A6.5 6.5 0 1 0 14.5 8 6.507 6.507 0 0 0 8 1.5Zm2.854 5.354-3.5 3.5a.502.502 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 9.293l3.146-3.147a.5.5 0 1 1 .708.708Z" />
+    </svg>
+  );
+};
+
+export const WarningCircle = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M8 1.5A6.5 6.5 0 1 0 14.5 8 6.507 6.507 0 0 0 8 1.5ZM7.5 5a.5.5 0 1 1 1 0v3.5a.5.5 0 1 1-1 0V5Zm.5 6.5A.75.75 0 1 1 8 10a.75.75 0 0 1 0 1.5Z" />
+    </svg>
+  );
+};
+
+export const Wrench = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M14.5 6a4.499 4.499 0 0 1-6.309 4.125l-3.253 3.764a2 2 0 1 1-2.853-2.803l.026-.024 3.764-3.253a4.504 4.504 0 0 1 5.221-6.174.5.5 0 0 1 .246.824L9 5l.354 1.647L11 7l2.54-2.345a.5.5 0 0 1 .825.246c.09.36.135.728.135 1.099Z" />
+    </svg>
+  );
+};
+
+export const Network = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M15 7.5a.5.5 0 0 1-.5.5h-2v2h.5a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1h.5V8h-7v2H5a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1h.5V8h-2a.5.5 0 1 1 0-1h6V5.5H7a1 1 0 0 1-1-1v-2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-.5V7h6a.5.5 0 0 1 .5.5Z" />
+    </svg>
+  );
+};
+
+export const Video = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M14.5 13a.5.5 0 0 1-.5.5H2a.5.5 0 0 1 0-1h12a.5.5 0 0 1 .5.5Zm0-9.5v7a1 1 0 0 1-1 1h-11a1 1 0 0 1-1-1v-7a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1ZM10.25 7a.5.5 0 0 0-.213-.41l-2.5-1.75a.5.5 0 0 0-.787.41v3.5a.5.5 0 0 0 .787.41l2.5-1.75A.5.5 0 0 0 10.25 7Z" />
+    </svg>
+  );
+};
+
+export const Barbell = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <g clipPath="url(#a)">
+        <path d="M12.5 4v8a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V8.5h-3V12a1 1 0 0 1-1 1h-1a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v3.5h3V4a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1Zm-10.25.5H2a1 1 0 0 0-1 1v2H.517A.511.511 0 0 0 0 7.967a.5.5 0 0 0 .5.533H1v2a1 1 0 0 0 1 1h.25a.25.25 0 0 0 .25-.25v-6.5a.25.25 0 0 0-.25-.25ZM16 7.967a.511.511 0 0 0-.516-.467H15v-2a1 1 0 0 0-1-1h-.25a.25.25 0 0 0-.25.25v6.5a.25.25 0 0 0 .25.25H14a1 1 0 0 0 1-1v-2h.5a.5.5 0 0 0 .5-.533Z" />
+      </g>
+      <defs>
+        <clipPath id="a">
+          <path fill="#fff" d="M0 0h16v16H0z" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export const MusicNote = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="m13.144 3.521-5-1.5A.5.5 0 0 0 7.5 2.5v6.766a3 3 0 1 0 1 2.234V6.172l4.356 1.307A.5.5 0 0 0 13.5 7V4a.5.5 0 0 0-.356-.479Z" />
+    </svg>
+  );
+};
+
+export const BookOpenText = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M14.5 3h-4a2 2 0 0 0-2 2v5.483a.51.51 0 0 1-.467.516.499.499 0 0 1-.533-.5V5a2 2 0 0 0-2-2h-4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5H6a1.5 1.5 0 0 1 1.5 1.496.494.494 0 0 0 .32.472.5.5 0 0 0 .68-.468A1.5 1.5 0 0 1 10 13h4.5a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5ZM13 10.5h-2.483a.51.51 0 0 1-.516-.467.499.499 0 0 1 .5-.533h2.483a.51.51 0 0 1 .516.467.5.5 0 0 1-.5.533Zm0-2h-2.483A.51.51 0 0 1 10 8.033a.499.499 0 0 1 .5-.533h2.483a.51.51 0 0 1 .516.467.5.5 0 0 1-.5.533Zm0-2h-2.483A.51.51 0 0 1 10 6.033a.5.5 0 0 1 .5-.533h2.483a.51.51 0 0 1 .517.467.5.5 0 0 1-.5.533Z" />
+    </svg>
+  );
+};
+
+export const ShieldPlus = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path
+        fill="#201F24"
+        d="M13 2.5H3a1 1 0 0 0-1 1V7c0 3.295 1.595 5.292 2.933 6.387 1.441 1.179 2.875 1.58 2.938 1.595a.5.5 0 0 0 .262 0c.063-.016 1.495-.416 2.938-1.595C12.405 12.292 14 10.295 14 7V3.5a1 1 0 0 0-1-1Zm-3 6H8.5V10a.5.5 0 0 1-1 0V8.5H6a.5.5 0 1 1 0-1h1.5V6a.5.5 0 1 1 1 0v1.5H10a.5.5 0 0 1 0 1Z"
+      />
+    </svg>
+  );
+};
+
+export const Warehouse = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M15 11.5h-.5V3.619l.604-.13a.5.5 0 1 0-.209-.978l-14 3a.5.5 0 0 0 .21.978l.395-.085V11.5H1a.5.5 0 0 0 0 1h14a.5.5 0 0 0 0-1Zm-3.5 0h-7v-1h7v1Zm0-2h-7v-1h7v1Z" />
+    </svg>
+  );
+};
+
+export const Filter = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="m14.238 4.173-.005.005L10 8.698v3.468a1 1 0 0 1-.446.834l-2 1.334A1 1 0 0 1 6 13.5V8.698l-4.233-4.52-.005-.005A1 1 0 0 1 2.5 2.5h11a1 1 0 0 1 .74 1.673h-.002Z" />
+    </svg>
+  );
+};
+
+export const Sort = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M13 2H3a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1ZM4.5 4.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 1 1 0-1Zm2.5 7H4.5a.5.5 0 0 1 0-1H7a.5.5 0 0 1 0 1Zm.5-3h-3a.5.5 0 1 1 0-1h3a.5.5 0 1 1 0 1Zm4.854 1.854-1.5 1.5a.503.503 0 0 1-.708 0l-1.5-1.5a.5.5 0 0 1 .708-.708l.646.647V7.5a.5.5 0 0 1 1 0v2.793l.646-.647a.5.5 0 0 1 .708.708Z" />
+    </svg>
+  );
+};
+
+export const Eye = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M15.457 7.798c-.022-.05-.551-1.224-1.728-2.401C12.16 3.829 10.18 3 8 3s-4.16.829-5.729 2.397C1.094 6.574.562 7.75.543 7.797a.5.5 0 0 0 0 .407c.022.05.551 1.223 1.728 2.4C3.84 12.17 5.82 13 8 13s4.16-.829 5.729-2.396c1.177-1.177 1.706-2.35 1.728-2.4a.5.5 0 0 0 0-.407ZM8 10.5a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5Z" />
+    </svg>
+  );
+};
+
+export const EyeSlash = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" {...props}>
+      <path d="M6.042 3.617a.25.25 0 0 1 .13-.413A8.125 8.125 0 0 1 8 3c2.18 0 4.16.829 5.729 2.397 1.177 1.177 1.706 2.351 1.728 2.4a.5.5 0 0 1 0 .407c-.022.05-.551 1.223-1.728 2.4a9.117 9.117 0 0 1-.55.504.25.25 0 0 1-.347-.022l-6.79-7.47Zm7.328 9.547a.5.5 0 1 1-.74.672l-1.38-1.515A7.95 7.95 0 0 1 8 13c-2.18 0-4.16-.829-5.729-2.396C1.094 9.427.562 8.254.543 8.204a.5.5 0 0 1 0-.407c.02-.047.551-1.223 1.728-2.4.47-.472.995-.887 1.563-1.236L2.63 2.836a.5.5 0 1 1 .74-.672l10 11Zm-4.093-3.016L5.984 6.523a2.5 2.5 0 0 0 3.293 3.625Z" />
+    </svg>
+  );
+};
