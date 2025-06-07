4a5d2c2541b4181b00bda3280ee66a3550e8bf96
Arda Eker <96771398+ardaeker@users.noreply.github.com>
2025-06-07 20:20:03 +0300

    update(layout): add shadow to container in SigninForm and SignupForm pages

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <section className="flex size-full items-center justify-center">
-      <div className="flex w-full max-w-lg flex-col gap-y-8 rounded-xl bg-white px-5 py-6 sm:p-8">
+      <div className="flex w-full max-w-lg flex-col gap-y-8 rounded-xl bg-white px-5 py-6 shadow-xs sm:p-8">
         <h1 className="text-3xl font-bold text-gray-900">Login</h1>
         <SigninForm />
         <p className="text-center text-sm text-gray-500">
