b89bc8fcf890a0d6712d8c78695e46369e1edab6
Arda Eker <96771398+ardaeker@users.noreply.github.com>
2025-06-07 22:29:37 +0300

    feat(layout): add padding to children container in layout component

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,7 +3,7 @@ import { MobileNavbar } from "@/components/mobile-navbar";
 export default function Layout({ children }: Layout) {
   return (
     <div>
-      <div>{children}</div>
+      <div className="pb-13 md:pb-18.5">{children}</div>
       <MobileNavbar />
     </div>
   );
