6c15f529ada5ced2575325cd8126913aca6b1cfc
Arda Eker <96771398+ardaeker@users.noreply.github.com>
2025-07-31 23:41:35 +0300

    fix(middleware): update session handling to use auth.getClaims() instead of auth.getUser()

--- src/utils/supabase/middleware/index.ts
+++ src/utils/supabase/middleware/index.ts
@@ -33,20 +33,33 @@ export async function updateSession(request: NextRequest) {
     // supabase.auth.getUser(). A simple mistake could make it very hard to debug
     // issues with users being randomly logged out.
 
-    // IMPORTANT: DO NOT REMOVE auth.getUser()
+    // IMPORTANT: DO NOT REMOVE auth.getClaims()
 
-    const {
-        data: { user },
-    } = await supabase.auth.getUser();
+    const { data } = await supabase.auth.getClaims();
+
+    const user = data?.claims;
 
     if (
         !user &&
-        !request.nextUrl.pathname.startsWith("/login") &&
+        !request.nextUrl.pathname.startsWith("/sign-in") &&
+        !request.nextUrl.pathname.startsWith("/sign-up") &&
+        !request.nextUrl.pathname.startsWith("/forgot-password") &&
         !request.nextUrl.pathname.startsWith("/auth")
     ) {
         // no user, potentially respond by redirecting the user to the login page
         const url = request.nextUrl.clone();
         url.pathname = "/login";
+        return NextResponse.redirect(url);
+    }
+
+    if (
+        user && !request.nextUrl.pathname.startsWith("/sign-in") &&
+        !request.nextUrl.pathname.startsWith("/sign-up") &&
+        !request.nextUrl.pathname.startsWith("/forgot-password") &&
+        !request.nextUrl.pathname.startsWith("/auth")
+    ) {
+        const url = request.nextUrl.clone();
+        url.pathname = "/";
         return NextResponse.redirect(url);
     }
 
